e2e1038eff9d189828ea93c8cdff28bee2996af7
Josué Ttito <josue.ttito@ucsp.edu.pe>
2019-02-12 18:40:30 -0500

    add videoreading

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "src/cube.hpp"
 #include "src/videoreading.hpp"
 
+using namespace std;
 
 int main() {
-    VideoReading * vr = new VideoReading();
+    string filename = "/home/josue/Documents/videos/cam1/anillos.mp4";
+    VideoReading * vr = new VideoReading(filename);
 
     return 0;
 }
--- src/videoreading.hpp
+++ src/videoreading.hpp
@@ -1,8 +1,41 @@
+#include "common.h"
 #include <opencv2/opencv.hpp>
 
-class VideoReading {
-public:
-    VideoReading() {
+using namespace std;
 
+class VideoReading {
+    int videoLength;
+    int videoWidth;
+    int videoHeight;
+
+    vector<cv::Mat> video;
+
+public:
+    VideoReading(string & filename) {
+        cv::VideoCapture capture;
+        capture.open(filename);
+        if (!capture.isOpened()) {
+            cerr << "Error processing file. Can't read video " << filename;
+        }
+        videoLength = static_cast<int>(capture.get(CV_CAP_PROP_FRAME_COUNT));
+        videoWidth = static_cast<int>(capture.get(CV_CAP_PROP_FRAME_WIDTH));
+        videoHeight = static_cast<int>(capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+
+        int step = 5;
+        cv::Mat image;
+
+        for (int i = 0; i < videoLength; i += step) {
+            capture.set(CV_CAP_PROP_POS_FRAMES, i);
+            capture.read((image));
+            if (image.empty()) {
+                std::cerr << "Error processing file. Can't read frame " << i << "from video %s" << filename;
+            }
+            video.push_back(image.clone());
+        }
+
+        cout << "videoLength: " << videoLength << endl;
+        cout << "videoWidth: " << videoWidth << endl;
+        cout << "videoHeight: " << videoHeight << endl;
     }
+
 };
