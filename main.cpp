0747ef3f4912e002f9dbd4089649851c115d34e5
Josue Ttito <josue.ttito@ucsp.edu.pe>
2019-02-12 20:43:34 -0500

    add dynamic video;

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 
-int main() {
-    string filename = "/home/josue/Documents/videos/cam1/anillos.mp4";
+int main(int argc, char *argv[]) {
+    cout << "You have entered " << argc << " arguments:" << "\n";
+    if (argc < 2) {
+        cerr << "Invalid video file path" << endl;
+        return 1;
+    }
+    string filename(argv[1]);
     VideoReading * vr = new VideoReading(filename);
 
     return 0;
