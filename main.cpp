fcc6f9212e29ad15b89d37de3fe1c541a25ea9e9
joshjo <josue.ttito@ucsp.edu.pe>
2019-02-13 12:45:56 -0500

    Initial OFCM

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "src/videoreading.hpp"
+#include "src/ofcm.hpp"
 
 
 using namespace std;
@@ -10,6 +11,32 @@ int main(int argc, char *argv[]) {
     }
     string filename(argv[1]);
     VideoReading * vr = new VideoReading(filename);
+
+    int nBinsMagnitude = 4;
+    int nBinsAngle = 6;
+    int distanceMagnitude = 2;
+    int distanceAngle = 3;
+    int cuboidLength = 5;
+    float maxMagnitude = 20;
+    int logQuantization = 0;
+
+    vector<int> tempScales;
+    tempScales.push_back(1); tempScales.push_back(2);
+
+
+    OFCM * ofcm = new OFCM(
+        nBinsMagnitude
+        , nBinsAngle
+        , distanceMagnitude
+        , distanceAngle
+        , cuboidLength
+        , maxMagnitude
+        , logQuantization
+        , tempScales
+    );
+
+    ofcm->setData(vr->video);
+    ofcm->preProcess();
 
     return 0;
 }
